5ba46ca454094667a1cc13b27d73758444ebf3c9
Ramon Encinas <ramon.enra@gmail.com>
2023-03-07 11:50:48 -0800

    Added loading message

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => { 
+       setTimeout(() => {
         async function fetchData() {
             const response = await fetch('http://localhost:8000/blogs');
             const data = await response.json();
             console.log(data);
             setBlogs(data);
+            setLoading(false);
         }
         fetchData();
+       }, 1000)
     }, []);
         
         
     return (
         <div className="home">
+            {loading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
