d97865987d6d61b17c789aea2c7f3c6fefda2254
Ramon Encinas <ramon.enra@gmail.com>
2023-03-08 13:45:29 -0800

    Created custom hook useFetch

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+          async function fetchData() {
+            const response = await fetch(url);
+            if (!response.ok) {
+              throw new Error("Something went wrong!");
+            }
+    
+            const data = await response.json();
+            console.log(response);
+            console.log(data);
+            setData(data);
+            setLoading(false);
+            setError(null);
+          }
+    
+          fetchData().catch((err) => {
+            setLoading(false);
+            setError(err.message);
+          });
+        }, 1000);
+      }, [url]);
+
+    return { data, loading, error };
+}
+
+export default useFetch;
