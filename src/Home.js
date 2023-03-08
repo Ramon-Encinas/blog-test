d20b84b74da5a7240497e7d4a7862cc40cd28d3a
Ramon Encinas <ramon.enra@gmail.com>
2023-03-08 12:52:45 -0800

    Error handling

--- src/Home.js
+++ src/Home.js
@@ -2,29 +2,40 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => { 
-       setTimeout(() => {
-        async function fetchData() {
-            const response = await fetch('http://localhost:8000/blogs');
-            const data = await response.json();
-            console.log(data);
-            setBlogs(data);
-            setLoading(false);
+  useEffect(() => {
+    setTimeout(() => {
+      async function fetchData() {
+        const response = await fetch("http://localhost:8000/blogs");
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
         }
-        fetchData();
-       }, 1000)
-    }, []);
-        
-        
-    return (
-        <div className="home">
-            {loading && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
-        </div>
-    );
-}
+
+        const data = await response.json();
+        console.log(response);
+        console.log(data);
+        setBlogs(data);
+        setLoading(false);
+        setError(null);
+      }
+
+      fetchData().catch((err) => {
+        setLoading(false);
+        setError(err.message);
+      });
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {loading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+};
 
 export default Home;
