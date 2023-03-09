206a46f19f20c3b75cc4ceb0d4cc1ca7741faed7
Ramon Encinas <ramon.enra@gmail.com>
2023-03-08 16:52:12 -0800

    Added funcionality to switch to different pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Counter from './Counter';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
-        <Counter />
+        <Switch>
+          <Route exact path={"/"}>
+            <Home />
+          </Route>
+          <Route path={"/create"}>
+            <Create />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
