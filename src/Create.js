f54b6eca79b8bb02a0a507b2ce81e23201a37f7b
Ramon Encinas <ramon.enra@gmail.com>
2023-03-14 17:54:45 -0700

    Programmatic Redirects

--- src/Create.js
+++ src/Create.js
@@ -1,36 +1,41 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('yoshi');
-  const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("yoshi");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const blog = {title, body, author};
+    const blog = { title, body, author };
 
     setLoading(true);
 
-    fetch('http://localhost:8000/blogs', {
-      method: 'POST',
-      headers: { "Content-Type": "application/json"},
-      body: JSON.stringify(blog) 
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
     }).then(() => {
-      console.log('new blog added')
-      setLoading(false)
-    })   
-  }
+      console.log("new blog added");
+      setLoading(false);
+      //history.go(-1);
+      history.push('/')
+    });
+
+  };
 
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
       <form onSubmit={handleSubmit}>
         <label>Blog title</label>
-        <input 
+        <input
           type="text"
-          required 
-          value ={title}
+          required
+          value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog body</label>
@@ -40,10 +45,7 @@ const Create = () => {
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        >
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
